4105da0e0b871fec05a10e040a37c119deddaea3
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-15 11:59:38 +0100

    display updates with numbers

--- calculator.js
+++ calculator.js
@@ -1,30 +1,52 @@
 const calculator = document.querySelector("#calculator");
 drawCalc();
+let num1 = 0;
+let operator = "";
+let operators = ["+", ,"-", "÷", "x", "!", "√", "x^2"]
 
 function drawCalc() {
     let counter = 0;
-    let buttons = ["!", "√", "x^2", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "C", "0", ".", "="]
-    for (let i = 0; i < 6; i++) {
+    let buttonContent = ["!", "√", "x^2", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "C", "0", ".", "="]
+    const row = document.createElement("row");
+    row.classList.add("row");
+    calculator.appendChild(row);
+    const display = document.createElement("display");
+    display.classList.add("display");
+    display.style.color = "dark-green";
+    display.textContent = 0;
+    row.appendChild(display);
+    for (let i = 1; i < 6; i++) {
         const row = document.createElement("row");
         row.classList.add("row");
         calculator.appendChild(row);
-        if (i > 0) {
-            for (let j = 0; j < 4; j++) {
-                const button = document.createElement("button");
-                button.classList.add("button");
-                button.style.color = "white";
-                button.textContent = buttons[counter];
-                row.appendChild(button);
-                counter ++;
-            }
+        for (let j = 0; j < 4; j++) {
+            const button = document.createElement("button");
+            button.classList.add("button");
+            button.style.color = "white";
+            button.textContent = buttonContent[counter];
+            button.onclick = function() {
+                selection = this.textContent;
+                if (selection == "c") {
+                    display.textContent = 0;
+                }
+                else if (isNumber(selection)) {
+                    if (display.textContent == "0") {
+                        display.textContent = selection;
+                    }
+                    else {
+                        display.textContent += selection;
+                    }
+                }
+            };
+            row.appendChild(button);
+            counter ++;
         }
-        else {
-            const display = document.createElement("display");
-            display.classList.add("display");
-            display.style.color = "dark-green";
-            display.textContent = 56350;
-            row.appendChild(display);
-        }
+    }
+}
+
+function isNumber (x) {
+    if (isNaN(x) == false) {
+        return true;
     }
 }
 
