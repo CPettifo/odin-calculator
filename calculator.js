ef77a538f3ce9441db6f1b2893aae7252ee5e07b
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-15 13:35:02 +0100

    finish addition functionality!

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
 const calculator = document.querySelector("#calculator");
 drawCalc();
-let num1, operator, num2;
-let operators = ["+", ,"-", "÷", "x", "!", "√", "x^2"]
+let num1 = 0, operator, num2 = 0;
+let operating = false;
+let operators = ["+", ,"-", "÷", "x"]
+let specials = ["!", "√", "x^2"]
 
 function drawCalc() {
     let counter = 0;
@@ -13,6 +15,7 @@ function drawCalc() {
     display.classList.add("display");
     display.style.color = "dark-green";
     display.textContent = 0;
+
     row.appendChild(display);
     for (let i = 1; i < 6; i++) {
         const row = document.createElement("row");
@@ -25,7 +28,6 @@ function drawCalc() {
             button.textContent = buttonContent[counter];
             button.onclick = function() {
                 selection = this.textContent;
-                
                 if (isNumber(selection) || selection == ".") {
                     if (display.textContent == "0") {
                         if (selection == ".") {
@@ -36,6 +38,10 @@ function drawCalc() {
                         }
                         
                     }
+                    else if (operating == true) {
+                        display.textContent = selection;
+                        operating = false;
+                    }
                     else {
                         display.textContent += selection;
                     }
@@ -44,10 +50,24 @@ function drawCalc() {
                 else {
                     if (selection == "C") {
                         display.textContent = 0;
-                        num1 = undefined;
+                        num1 = 0;
                     }
-                    if (operators.contains(selection)){
-                        display.textContent = "operate";
+                    if (specials.includes(selection)) {
+                        display.textContent = operate(selection, display.textContent);
+                    }
+                    if (operators.includes(selection)){
+                        
+                        if (num1 == 0) {
+                            operator = selection;
+                            num1 = display.textContent;
+                            console.log("Operator: " + operator + " number: " + num1);
+                            
+                        }
+                        else {
+                            display.textContent = operate(operator, num1, display.textContent);
+                            num1 = display.textContent;
+                        }
+                        operating = true;
                     }
 
                 }
@@ -56,6 +76,20 @@ function drawCalc() {
             counter ++;
         }
     }
+}
+
+function operate (operator, x, y = undefined) {
+    let result;
+    if (operator == "!") {
+        result = factorial (+x);
+    }
+    else if (operator == "+") {
+        result = add(+x, +y);
+    }
+    if (result > 999999999999999999) {
+        result = "ERROR";
+    }
+    return result;
 }
 
 function isNumber (x) {
