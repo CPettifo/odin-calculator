4170c4d107758b5f58b8edc040714b8963e2a478
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-15 10:35:22 +0100

    refactor and refine code

--- calculator.js
+++ calculator.js
@@ -1,58 +1,30 @@
+const calculator = docuent.querySelector("#calcultor");
+drawCalc();
 
+function drawCalc() {
 
+}
 
-const add = function(x, y) {
-    return x + y;
-  };
+function add(x, y) {
+   return x + y;
+};
   
-  const subtract = function(x, y) {
-      return x - y;
-  };
+function subtract(x, y) {
+  return x - y;
+};
   
-  const sum = function(array) {
-    let sum = 0;
-      for (let x of array) {
-      x = +x;
-      sum += x;
-    }
-    return sum;
-  };
+function multiply(x, y) {
+  return x * y;
+};
   
-  const multiply = function(array) {
-    let result = 1;
-    for (let x of array) {
-      x = +x;
-      result *= x;
-    }
-    return result;
-  };
+function power(x, y) {
+  let result = 1;
+  for (let i = 0; i < y; i++) {
+    result *= x;
+  }
+  return result;
+};
   
-  const power = function(x, y) {
-    let result = 1;
-    for (let i = 0; i < y; i++) {
-      result *= x;
-    }
-    return result;
-  };
-  
-  const factorial = function(x) {
-      let result = 1;
-    if (x == 0) {
-      return 1;
-    }
-    for (let i = 1; i < x + 1; i++) {
-      result *= i;
-    }
-    return result;
-  };
-  
-  // Do not edit below this line
-  module.exports = {
-    add,
-    subtract,
-    sum,
-    multiply,
-    power,
-    factorial
-  };
-  
+function divide(x, y) {
+  return x / y;
+}
