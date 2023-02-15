d3c7d362db04e94e2254416fa8139b19b06a5552
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-15 11:18:26 +0100

    functionless buttons place

--- calculator.js
+++ calculator.js
@@ -1,8 +1,33 @@
-const calculator = docuent.querySelector("#calcultor");
+const calculator = document.querySelector("#calculator");
 drawCalc();
 
 function drawCalc() {
-
+    let counter = 0;
+    let buttons = ["!", "√", "x^2", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "C", "0", ".", "="]
+    for (let i = 0; i < 6; i++) {
+        const row = document.createElement("row");
+        row.classList.add("row");
+        row.style.backgroundColor = "black";
+        calculator.appendChild(row);
+        if (i > 0) {
+            for (let j = 0; j < 4; j++) {
+                const button = document.createElement("button");
+                button.classList.add("button");
+                button.style.color = "white";
+                button.style.backgroundColor = "grey";
+                button.textContent = buttons[counter];
+                row.appendChild(button);
+                counter ++;
+            }
+        }
+        else {
+            const display = document.createElement("display");
+            display.classList.add("display");
+            display.style.color = "dark-green";
+            display.textContent = 56350;
+            row.appendChild(display);
+        }
+    }
 }
 
 
